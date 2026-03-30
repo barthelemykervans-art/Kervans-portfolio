414ee694de22810b740442b21fe26d3bdde4ee73
barthelemykervans-art <barthelemykervans@gmail.com>
2026-03-30 00:11:04 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Kervans Barthelemy</h1>
+      <p>Video Editor • Sound Design • Motion Graphics</p>
+      <p>Portfolio site coming soon.</p>
+    </main>
+  );
+}
